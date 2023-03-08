757957273f6a0ddc236610202be54e5a18c3251e
KeyaAkter <sanhatanvin12@gmail.com>
2023-03-08 22:38:43 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
