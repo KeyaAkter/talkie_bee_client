f44474eb6733eb36d25256cf0147c916bc16d980
KeyaAkter <sanhatanvin12@gmail.com>
2023-03-08 22:50:46 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="py-10 flex justify-between">
+      <div className="logo">
+        <Link
+          to="/"
+          className="text-xl font-semibold hover:text-violet-500 duration-300"
+        >
+          TalkieBee
+        </Link>
+      </div>
+      <nav>
+        <div className="flex gap-5 ">
+          <Link
+            to="/login"
+            className="hover:text-violet-500 hover:underline underline-offset-2 duration-300"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="hover:text-violet-500 hover:underline underline-offset-2 duration-300"
+          >
+            Register
+          </Link>
+        </div>
+        {/* <div>
+          <p>
+            Logged in as : <span className="text-violet-500">Tanisha</span>
+          </p>
+        </div> */}
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
